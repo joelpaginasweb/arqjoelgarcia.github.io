d7376b8577b9c668226d45d82ef9fab6ef6ed3ea
theJoelGs <53578763+joelpaginasweb@users.noreply.github.com>
2023-05-19 10:31:14 -0500

    Add files via upload
    subi archivos de pagina

---
+++ javas/jscript.js
@@ -0,0 +1,13 @@
+function abrirmenu()
+{
+   ele =document.getElementById("menu");
+    if((ele.style.transform=="translateX(-100%)") || (ele.style.transform==""))
+    {
+        ele.style.transform="translateX(0%)";
+    }
+    else 
+    {
+       ele.style.transform="translateX(-100%)";
+    }
+}
+
